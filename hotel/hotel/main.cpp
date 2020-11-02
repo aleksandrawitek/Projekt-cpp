8b08d6f992493f3655d71ca69ac61dffd9d8e3c0
Aleksandra Witek <olciawitek@wp.pl>
2020-11-02 23:41:06 +0100

    Raport 1

--- hotel/hotel/main.cpp
+++ hotel/hotel/main.cpp
@@ -1,91 +1,108 @@
-
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
-//
-
-#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <iostream>
 
-// Here is a small helper for you! Have a look.
-#include "ResourcePath.hpp"
-
-int main(int, char const**)
+int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
+    //pomocnicze zmienne na szerokość i długość ekranu
+    float a;
+    float b;
+    
+    a = sf::VideoMode::getDesktopMode().width;
+    b = sf::VideoMode::getDesktopMode().height;
+    
+    //wygenerowanie okna dopasowanego do rozmiaru ekranu
+    sf::RenderWindow App(sf::VideoMode(a, b), "System hotelowy");
+    
+    //dodanie ikon
+    
+    //ikona kartoteka
     sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
+    if (!texture.loadFromFile("/Users/aleksandrawitek/Documents/Projekt cpp/Projekt-cpp/hotel/hotel/logo/kartoteka.png")) {
+      std::cout << "Could not load enemy texture" << std::endl;
+      return 0;
     }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
+    sf::Sprite kartoteka;
+    kartoteka.setTexture(texture);
+    kartoteka.setPosition(sf::Vector2f(a/10,b/10));
+    
+    //ikona check in
+    sf::Texture texture1;
+    if (!texture1.loadFromFile("/Users/aleksandrawitek/Documents/Projekt cpp/Projekt-cpp/hotel/hotel/logo/checkin.png")) {
+      std::cout << "Could not load enemy texture" << std::endl;
+      return 0;
     }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
+    sf::Sprite checkin;
+    checkin.setTexture(texture1);
+    checkin.setPosition(sf::Vector2f(a*0.4,b/10));
+    
+    //ikona check out
+      sf::Texture texture2;
+      if (!texture2.loadFromFile("/Users/aleksandrawitek/Documents/Projekt cpp/Projekt-cpp/hotel/hotel/logo/checkout.png")) {
+        std::cout << "Could not load enemy texture" << std::endl;
+        return 0;
+      }
+      sf::Sprite checkout;
+      checkout.setTexture(texture2);
+      checkout.setPosition(sf::Vector2f(a*0.7,b/10));
+    
+    //ikona platnosc
+       sf::Texture texture4;
+       if (!texture4.loadFromFile("/Users/aleksandrawitek/Documents/Projekt cpp/Projekt-cpp/hotel/hotel/logo/platnosc.png")) {
+         std::cout << "Could not load enemy texture" << std::endl;
+         return 0;
+       }
+       sf::Sprite platnosc;
+       platnosc.setTexture(texture4);
+    platnosc.setPosition(sf::Vector2f(a/10,b*0.5));
+       
+       //ikona status
+       sf::Texture texture5;
+       if (!texture5.loadFromFile("/Users/aleksandrawitek/Documents/Projekt cpp/Projekt-cpp/hotel/hotel/logo/status.png")) {
+         std::cout << "Could not load enemy texture" << std::endl;
+         return 0;
+       }
+       sf::Sprite status;
+       status.setTexture(texture5);
+       status.setPosition(sf::Vector2f(a*0.4,b*0.5));
+       
+       //ikona parking
+         sf::Texture texture6;
+         if (!texture6.loadFromFile("/Users/aleksandrawitek/Documents/Projekt cpp/Projekt-cpp/hotel/hotel/logo/parking.png")) {
+           std::cout << "Could not load enemy texture" << std::endl;
+           return 0;
+         }
+         sf::Sprite parking;
+         parking.setTexture(texture6);
+         parking.setPosition(sf::Vector2f(a*0.7,b*0.5));
+      
 
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
+    //program trwa do zamknięcia okna
+    while (App.isOpen())
     {
-        // Process events
         sf::Event event;
-        while (window.pollEvent(event))
+        while (App.pollEvent(event))
         {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                App.close();
         }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
+        
+        //zmiana koloru tła
+        App.clear(sf::Color::White);
+        
+        //wyświetlenie ikon
+        App.draw(kartoteka);
+        App.draw(checkin);
+        App.draw(checkout);
+        App.draw(status);
+        App.draw(parking);
+        App.draw(platnosc);
+        App.display();
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
